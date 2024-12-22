e073e2bc48d2c20b5a91303bb44717efc0b57837
Worestesa <Worestesa@users.noreply.github.com>
2024-12-22 14:57:44 +0300

    Финал

---
+++ Assets/MenuButten.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+public class MenuButten : MonoBehaviour
+{
+    [SerializeField] Button button1;
+    [SerializeField] Button button2;
+    [SerializeField] Button button3;
+    [SerializeField] GameObject imaig1;
+    [SerializeField] GameObject imaig2;
+    [SerializeField] GameObject imaig3;
+    
+    
+}
